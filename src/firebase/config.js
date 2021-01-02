8d546dedf31bcc87da48bf35a0812601486f1439
otyazukeGit <otyazuke.gookle@gmail.com>
2021-01-02 20:53:16 +0900

    fix the format

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-	apiKey: "AIzaSyDoq3sLtwkr5-I3mUJXQ2fWk9tjMwlW0Kg",
-	authDomain: "marubatsugame-36453.firebaseapp.com",
-	databaseURL: "https://marubatsugame-36453.firebaseio.com",
-	projectId: "marubatsugame-36453",
-	storageBucket: "marubatsugame-36453.appspot.com",
-	messagingSenderId: "182025569254",
-	appId: "1:182025569254:web:e5cdb62ce839eb9bf60c27",
-	measurementId: "G-5VPFTF8XF6"
-};
+  apiKey: 'AIzaSyDoq3sLtwkr5-I3mUJXQ2fWk9tjMwlW0Kg',
+  authDomain: 'marubatsugame-36453.firebaseapp.com',
+  databaseURL: 'https://marubatsugame-36453.firebaseio.com',
+  projectId: 'marubatsugame-36453',
+  storageBucket: 'marubatsugame-36453.appspot.com',
+  messagingSenderId: '182025569254',
+  appId: '1:182025569254:web:e5cdb62ce839eb9bf60c27',
+  measurementId: 'G-5VPFTF8XF6',
+}
